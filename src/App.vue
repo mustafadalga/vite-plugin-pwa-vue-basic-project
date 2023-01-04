ccd3f060d404654af3211c2d6ec8d2fa1d9890e3
Mustafa Dalga <mustafadalgaa@gmail.com>
2023-01-04 22:42:15 +0300

    cleanup

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { useTimeAgo } from '@vueuse/core'
+import ReloadPrompt from './ReloadPrompt.vue'
+
+// replaced dyanmicaly
+const date = '__DATE__'
+const timeAgo = useTimeAgo(date)
+</script>
+
+<template>
+  <div>Built at: {{ date }} ({{ timeAgo }})</div>
+  <h1>Deployment number:2</h1>
+  <ReloadPrompt />
+</template>
